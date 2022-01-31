43409e3291140ead1d2ae4ede813f0f965a4fda4
eval init <evalinit@gmail.com>
2022-01-31 00:40:09 -0700

    put conn back on the window

--- sample-server/index.js
+++ sample-server/index.js
@@ -1,14 +1,16 @@
+document.body.style.margin = '0'
 document.body.style.height = '100%'
 document.documentElement.style.height = '100%'
 
 document.body.innerHTML = `
-  <iframe width="100%" height="100%" src="//jsfiddle.net/evalinit/5h6quy37/embedded/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
+  <iframe width="100%" height="100%" src="//jsfiddle.net/${localStorage.getItem('firstFiddle')}/embedded/js,html,css,result/dark/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
 `
 
-window.addEventListener('message', function (evt) {
-  console.log(evt)
-  if (evt.data.type == 'launchFiddle') {
-    document.write(evt.data.document)
-    window.removeEventListener('message', this)
+const listenForChildDocument = (event) => {
+  if (event.data.type == 'launchFiddle') {
+    window.removeEventListener('message', listenForChildDocument)
+    document.write(event.data.document)
   }
-})
+}
+
+window.addEventListener('message', listenForChildDocument)
